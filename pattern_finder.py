58e28eb3c815f6432113ec1815f771274da18ec2
julian torres <juliannogueira@yahoo.com>
2018-05-20 22:23:34 -1000

    Additional code was added to search for phone number patterns in strings of data.

--- pattern_finder.py
+++ pattern_finder.py
@@ -25,3 +25,21 @@ print(is_phone_number('012-345-6789'))
 
 print('0123456789 is a phone number.')
 print(is_phone_number('0123456789'))
+
+
+
+
+#This next block of code adds more functionality to pattern_finder. It actually looks through
+#entire strings of data, and it reports if there are any phone numbers found in it - based on 
+#what qualifies as a phone number per the is_phone_number function. 
+
+test_string = 'Please call me on my cellphone: 619-782-6610, or you can reach me from 0900-1700 on my work line: 619-454-0189.'    #This string will be tested.
+
+def pattern_finder(a_string):
+    for y in range(len(a_string)):
+        data_chunk = a_string[y: y + 12]    #This slices a string at each index, (12) char. long. 
+
+        if is_phone_number(data_chunk) == True:    #Here we run our test function on the string.
+            print('Phone number found: ' + data_chunk)
+
+pattern_finder(test_string)
